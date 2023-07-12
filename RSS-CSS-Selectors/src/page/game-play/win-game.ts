1091d8aa39885203cee1b2cf6f3041037f860d70
Andrey Nezhdanov <nezhd.an@yandex.ru>
2023-07-13 00:48:09 +0300

    refactor: improve win-game.ts

--- RSS-CSS-Selectors/src/page/game-play/win-game.ts
+++ RSS-CSS-Selectors/src/page/game-play/win-game.ts
@@ -2,16 +2,13 @@ import levelRender from "../levels/level-render";
 import resetProgress from "./reset-progress";
 
 export default function winGame() {
-  if (localStorage.getItem('level1') == 'completed' 
-  && localStorage.getItem('level2') == 'completed' 
-  && localStorage.getItem('level3') == 'completed' 
-  && localStorage.getItem('level4') == 'completed' 
-  && localStorage.getItem('level5') == 'completed' 
-  && localStorage.getItem('level6') == 'completed' 
-  && localStorage.getItem('level7') == 'completed' 
-  && localStorage.getItem('level8') == 'completed' 
-  && localStorage.getItem('level9') == 'completed'  
-  && localStorage.getItem('level10') == 'completed') {
+  const stateLevels = [localStorage.getItem('level1'), localStorage.getItem('level2'), localStorage.getItem('level3'), localStorage.getItem('level4'), localStorage.getItem('level5'), localStorage.getItem('level6'), localStorage.getItem('level7'), localStorage.getItem('level8'), localStorage.getItem('level9'), localStorage.getItem('level10')];
+  
+  function isCompleted(element: string): boolean {
+    return element == 'completed';
+  }
+
+  if ((stateLevels as string[]).every(isCompleted)) {
     const container = document.querySelector('.win') as HTMLElement;
     const popup = document.querySelector('.popup') as HTMLElement;
     const restartButton = document.querySelector('.restart-button') as HTMLElement;
@@ -19,7 +16,7 @@ export default function winGame() {
     container.classList.add('win-active');
     popup.classList.add('popup-active');
     document.body.style.overflow = 'hidden';
-
+    
     restartButton.addEventListener('click', () => {
       container.classList.remove('win-active');
       popup.classList.remove('popup-active');
