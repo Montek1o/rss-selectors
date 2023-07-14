bbbde430c3bb277aacf22f172c18d6b74416c1f4
Andrey Nezhdanov <nezhd.an@yandex.ru>
2023-07-14 13:44:42 +0300

    refactor: win-game.ts

--- RSS-CSS-Selectors/src/page/game-play/win-game.ts
+++ RSS-CSS-Selectors/src/page/game-play/win-game.ts
@@ -2,13 +2,13 @@ import levelRender from "../levels/level-render";
 import resetProgress from "./reset-progress";
 
 export default function winGame() {
-  const stateLevels: Array<string> = [localStorage.getItem('level1'), localStorage.getItem('level2'), localStorage.getItem('level3'), localStorage.getItem('level4'), localStorage.getItem('level5'), localStorage.getItem('level6'), localStorage.getItem('level7'), localStorage.getItem('level8'), localStorage.getItem('level9'), localStorage.getItem('level10')] as Array<string>;
+  const stateLevels = [localStorage.getItem('level1'), localStorage.getItem('level2'), localStorage.getItem('level3'), localStorage.getItem('level4'), localStorage.getItem('level5'), localStorage.getItem('level6'), localStorage.getItem('level7'), localStorage.getItem('level8'), localStorage.getItem('level9'), localStorage.getItem('level10')];
   
   function isCompleted(element: string): boolean {
     return element == 'completed';
   }
 
-  if (stateLevels.every(isCompleted)) {
+  if ((stateLevels as Array<string>).every(isCompleted)) {
     const container = document.querySelector('.win') as HTMLElement;
     const popup = document.querySelector('.popup') as HTMLElement;
     const restartButton = document.querySelector('.restart-button') as HTMLElement;
