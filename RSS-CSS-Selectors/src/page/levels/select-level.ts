3ed409ddc7deca14df8391f2e6eface8935558e6
Andrey Nezhdanov <nezhd.an@yandex.ru>
2023-10-11 01:19:47 +0300

    feat: deploy task

--- RSS-CSS-Selectors/src/page/levels/select-level.ts
+++
@@ -1,9 +0,0 @@
-export default function selectLevel(levelId: string): void {
-  const levelNumber = document.querySelector(`#level-${levelId}`) as HTMLElement;
-  const levelsNumber = document.querySelectorAll('.levels__item');
-
-  levelsNumber.forEach(e => {
-    e.classList.remove('levels__item-active');
-  })
-  levelNumber.classList.add('levels__item-active');
-}
